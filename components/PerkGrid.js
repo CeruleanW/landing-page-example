33e3bf9843a54404c9175227247434a596aea83f
CeruleanW <yyang.techie@gmail.com>
2021-03-09 11:44:29 -0500

    start layout

--- components/PerkGrid.js
+++ components/PerkGrid.js
@@ -3,22 +3,22 @@ import { Typography } from '@material-ui/core';
 import styled from 'styled-components';
 import { useTheme } from '@material-ui/core/styles';
 
+const Heading = styled(Typography)`
+  color: ${props => props.headingColor};
+`;
+
+const Body = styled(Typography)`
+  color: ${props => props.bodyColor};
+`;
+
 export default function PerkGrid(props) {
-  const {headingColor, bodyColor, heading, body} = props;
-  const Heading = styled(Typography)`
-    color: ${headingColor};
-  `;
-
-  const Body = styled(Typography)`
-    color: ${bodyColor}
-  `;
-
+  const { headingColor, bodyColor, heading, body } = props;
   return (
     <div>
       <Heading>{heading}</Heading>
       <Body>{body}</Body>
     </div>
-  )
+  );
 }
 
 /* <Line /> */
